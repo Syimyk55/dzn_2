a7fa93053751e22e908d63ecd912c5be378043e6
Syimyk55 <114605451+Syimyk55@users.noreply.github.com>
2022-09-30 17:53:33 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+     String name = "Syimyk";
+
+   int age =18;
+   int temperature = 20;
+     if (age <20 ||age > 45 && temperature > -20 || temperature< 35)
+         System.out.println("Можно идти гулять");
+if (age > 15    && temperature >16);
+        System.out.println("Можно идти гулять");
+     if (age < 45 && temperature> -5 );
+        System.out.println("Можно идти гулять");
+if ( age < 65 && temperature >-20);
+        System.out.println("Оставайтесь дома");
+
+
+     }
+ }
